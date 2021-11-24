56f50792d6d9e8fcda863329bcab9c9284c62d84
Natalia Vlasenko <Natalija.Vlasenko.01@privatbank.ua>
2021-11-24 23:32:26 +0200

    init commit 92

--- src/com/pb/vlasenko/HW9/FileNumbers.java
+++ src/com/pb/vlasenko/HW9/FileNumbers.java
@@ -2,29 +2,20 @@ package com.pb.vlasenko.HW9;
 import java.io.*;
 import java.util.Scanner;
 import java.io.FileReader;
-import java.io.Reader;
 
 public class FileNumbers {
 
-
     public static void main(String[] args) throws Exception {
-
         int[][] mas = new int[10][10];
-        for (int i = 0; i < 10; i++) {
-            for (int j = 0; j < 10; j++) {
+        int i;
+        int j;
+        for (i = 0; i < 10; i++)
+            for (j = 0; j < 10; j++)
                 mas[i][j] = (int) (Math.random() * 99) + 1;
-            }
-            Scanner scan = new Scanner(System.in);
 
-            for (int j = 0; j < 10; j++) {
-                System.out.print(mas[i][j] + " ");
-
-            }
-
-            System.out.println();
-        }
         createNumbersFile(mas, "D:/numbers.txt");
-        createOddNumbersFile("D:/numbers.txt", "D:/odd-numbers.txt");
+        createOddNumbersFile();
+
 
 
     }
@@ -32,57 +23,77 @@ public class FileNumbers {
     private static void createNumbersFile(int[][] mas, String s) {
         try {
             BufferedWriter bw = new BufferedWriter(new FileWriter(s));
-            bw.write(String.valueOf(10));
+
+            mas = new int[10][10];
+            //Random random = new Random();
+            System.out.println("Создан файл: ");
+
             bw.newLine();
-           bw.write(String.valueOf(10));
-            bw.newLine();
+
             for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++)
-                {
-                    mas[i][j] = (int) (Math.random() * 99) + 1;
-                }
-                Scanner scan = new Scanner(System.in);
                 for (int j = 0; j < 10; j++) {
-                }
-                {for (int j = 0; j < 10; j++)
-                    bw.write(String.valueOf(mas[i][j]));
-                    bw.newLine();
+                    mas[i][j] = (int) (Math.random() * 99) + 1;
+                    bw.write(mas[i][j] + "\n");
 
                 }
+                Scanner scan = new Scanner(System.in);
+
+                for (int j = 0; j < 10; j++) {
+                    System.out.print(mas[i][j] + " ");
+
+                }
+
+                System.out.println();
+                bw.append("\n");
             }
+            bw.newLine();
+            Scanner scan = new Scanner(System.in);
+
+
             bw.flush();
             bw.close();
+
         } catch (IOException e) {
             e.printStackTrace();
         }
     }
 
-    private static int[][] createOddNumbersFile(String s, String SS) {
-        int[][] mas = null;
-        try (BufferedReader br = new BufferedReader(new FileReader(s))) {
-            BufferedWriter bww = new BufferedWriter(new FileWriter(SS));
-          int couls = Integer.parseInt(br.readLine());
-           int rouls = Integer.parseInt(br.readLine());
-            mas = new int[10][10];
-            for (int i = 0; i < 10; i++) {
-                for (int j = 0; j < 10; j++)
-                    if (i % 2 == 0) if (j % 2 == 0)
-                       //System.out.print(mas[i][j] + " ");
 
-                        bww.write(String.valueOf(mas[i][j]));
-                for (int j = 0; j < 10; j++)
-                       mas[i][j] = Integer.parseInt(br.readLine());
-                bww.newLine();
+    public static void createOddNumbersFile() {
+        try {
+            try (Scanner in = new Scanner(new File("D:/numbers.txt"));
+                 PrintWriter out = new PrintWriter(new FileWriter("odd-numbers.txt"));) {
+                System.out.println("Вывод, с учетом замены целых четных чисел на '0':");
+                while (in.hasNextLine()) {
+                    Scanner line = new Scanner(in.nextLine());
+                    while (line.hasNextInt()) {
+                        int data = line.nextInt();
+                        if (data % 2 == 0) {
+                            System.out.format("%d -> 0, ", data);
+                            data = 0;
+                        } else
+                            System.out.print(data + ", ");
+                        out.print(data + ", ");
+                    }
+                    line.close();
+                    out.println();
+                    System.out.println();
+                }
+            } catch (FileNotFoundException e) {
+                {
+                    System.out.println("FileNotFoundException: " + e);
+                    e.printStackTrace();
+                }
             }
-            bww.flush();
-            bww.close();
         } catch (IOException e) {
             e.printStackTrace();
-        }catch (Exception e){
-            e.printStackTrace();
+        }
     }
-        return mas;
-}}
+}
+
+
+
+
 
 
 
